8aa2365a0779fe58c50fac7caa5ca3c6f33edd7c
Thuparshireesha <129254092+Thuparshireesha@users.noreply.github.com>
2023-05-20 18:06:15 +0530

    Add files via upload

---
+++ trip.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Document</title>
+    <style>
+      
+        body{
+        
+    background-image:url("https://img.freepik.com/premium-vector/road-trip-vacation-by-car-highway-with-beach-hills-view-concept-cartoon-illustration_338371-682.jpg?w=2000");
+    background-size:cover;
+    background-repeat:no-reapeat;
+
+        }
+        h1{
+          font-style:italic;
+          color:wight;
+        }
+</style>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm navbar-dark bg-warning">
+  <div class="container">
+    <h1 class="float-center">Travel with friends</h1>
+      <ul>
+      <div class="collapse navbar-collapse" id="mynavbar">
+      <form class="d-flex">
+  
+    <a  href="index.php" class="btn btn-primary">Admin</a>
+    <a href="student-create.php" class="btn btn-primary">User</a>
+      </ul>
+    
+      </nav>
+            
+</body>
+</html>
